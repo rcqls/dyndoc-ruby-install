2302783628664418cd08f6bda056a01f798569e9
R cqls <rdrouilh@gmail.com>
2014-10-29 17:34:40 +0100

    Gemdir.user in PATH

--- bin/install.sh
+++ bin/install.sh
@@ -4,29 +4,72 @@ echo check git
 if [ $(which git) == "" ]; then
 	echo "git is not installed!" 
  	exit
+else
+	echo ok
+fi
+
+echo check ruby
+if [ $(which ruby) == "" ]; then
+	echo "ruby is not installed!" 
+ 	exit
+else
+	echo ok
+fi
+
+echo check R
+if [ $(which R) == "" ]; then
+	echo "R is not installed!" 
+ 	exit
+else
+	echo ok
 fi
 
 echo check gem
 if [ $(which gem) == "" ]; then
 	echo "gem is not installed!" 
  	exit
+else
+	echo ok
 fi
 
 echo check atom
 if [ $(which atom) == "" ]; then
 	echo "atom is not installed!" 
  	exit
+else
+	echo ok
 fi
 
 echo check apm
 if [ $(which apm) == "" ]; then
 	echo "apm is not installed!" 
  	exit
+else
+	echo ok
 fi
 
 mkdir -p ../install
 echo install bundler gem
 gem install bundler --no-ri --no-rdoc
+
+echo check bundle
+if [ $(which bundle) == "" ]; then
+	echo "bundle is not installed!"
+ 	read -p "Add `ruby -e 'print Gem.user_dir'`/bin to PATH in ~/.bash_profile? [Y/N]" -n 1 -r
+	echo    # (optional) move to a new line
+	if [[ $REPLY =~ ^[Yy]$ ]]
+	then
+		`cat bash_profile` >> ${HOME}/.bash_profile
+		. ${HOME}/.bash_profile
+	fi
+fi
+echo recheck bundle
+if [ $(which bundle) == "" ]; then
+	echo "bundle is not installed!" 
+ 	exit
+else
+	echo ok
+fi
 echo install dyndoc dependencies
 bundle install
 echo install rb4R R package
