0799211a3ce418c22fd04e53df12060629079bc3
R cqls <rdrouilh@gmail.com>
2014-10-29 18:40:16 +0100

    some fix

--- bin/install.sh
+++ bin/install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+#!/bin/bash
 
 echo "check git in PATH"
-if [ $(which git) = "" ]; then
+if [ "$(which git)" = "" ]; then
 	echo "git is not installed!" 
  	exit
 else
@@ -9,7 +10,7 @@ else
 fi
 
 echo "check ruby in PATH"
-if [ $(which ruby) = "" ]; then
+if [ "$(which ruby)" = "" ]; then
 	echo "ruby is not installed!" 
  	exit
 else
@@ -17,7 +18,7 @@ else
 fi
 
 echo "check R in PATH"
-if [ $(which R) = "" ]; then
+if [ "$(which R)" = "" ]; then
 	echo "R is not installed!" 
  	exit
 else
@@ -25,7 +26,7 @@ else
 fi
 
 echo "check gem in PATH"
-if [ $(which gem) = "" ]; then
+if [ "$(which gem)" = "" ]; then
 	echo "gem is not installed!" 
  	exit
 else
@@ -33,7 +34,7 @@ else
 fi
 
 echo "check atom in PATH"
-if [ $(which atom) = "" ]; then
+if [ "$(which atom)" = "" ]; then
 	echo "atom is not installed!" 
  	exit
 else
@@ -41,7 +42,7 @@ else
 fi
 
 echo "check apm in PATH"
-if [ $(which apm) = "" ]; then
+if [ "$(which apm)" = "" ]; then
 	echo "apm is not installed!" 
  	exit
 else
@@ -49,24 +50,25 @@ else
 fi
 
 mkdir -p ../install
-if [ $(gem which bundler) == "" ]; then
+if [ "$(gem which bundler)" = "" ]; then
 	echo install bundler gem
 	gem install bundler --no-ri --no-rdoc
 fi
 
 echo check bundle in PATH
-if [ $(which bundle) = "" ]; then
+if [ "$(which bundle)" = "" ]; then
 	echo "bundle is not installed!"
  	read -p "Add `ruby -e 'print Gem.user_dir'`/bin to PATH in ~/.bash_profile? [Y/N]" -n 1 -r
 	echo    # (optional) move to a new line
 	if [[ $REPLY =~ ^[Yy]$ ]]
 	then
-		`cat bash_profile` >> ${HOME}/.bash_profile
+		echo "## added automatically when installing dyndoc ruby" >>  ${HOME}/.bash_profile
+		echo `cat bash_profile` >> ${HOME}/.bash_profile
 		. ${HOME}/.bash_profile
 	fi
 fi
 echo recheck bundle
-if [ $(which bundle) = "" ]; then
+if [ "$(which bundle)" = "" ]; then
 	echo "bundle is not installed!" 
  	exit
 else
@@ -85,3 +87,4 @@ git clone https://github.com/rcqls/dyndoc-syntax.git
 git clone https://github.com/rcqls/atom-dyndoc-viewer.git
 apm link dyndoc-syntax/atom/language-dyndoc
 apm link atom-dyndoc-viewer
+
