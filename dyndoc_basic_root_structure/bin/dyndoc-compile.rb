7a62d27cd0bed893bef54a67ea1a2e463c5f3d46
R cqls <rdrouilh@gmail.com>
2014-11-03 12:33:57 +0100

    -c and -d options

--- dyndoc_basic_root_structure/bin/dyndoc-compile.rb
+++ dyndoc_basic_root_structure/bin/dyndoc-compile.rb
@@ -1,10 +1,36 @@
 #!/usr/bin/env ruby
-
 require 'dyndoc/document'
 
 ## p Dyndoc.cfg_dir
 
+require 'optparse'
 
-d=Dyndoc::TemplateDocument.new(ARGV[0])
+OptionParser.new do |opts|
+  opts.banner = "Usage: dyndoc-compile.rb [options]"
+
+  opts.on("-d", "--docs one,two,three", Array, "list of documents to compile") do |list|
+    Settings["cfg_dyn.doc_list"] = list
+  end
+
+  opts.on('-c', '--cmd COMMAND','[s(ave)][b(ackup)][c(at)][pdf|png][v(iew)] and x=sv') {|c|
+    cmd =[]
+    cmd << :save if c.include? "s"
+    cmd << :backup if c.include? "b"
+    cmd << :cat if c.include? "c"
+    cmd << :pdf if c.include? "pdf"
+    cmd << :png if c.include? "png"
+    cmd << :view if c.include? "v"
+    cmd << :save << :view if c.include? "x"
+    cmd =[:cat] if cmd.empty? #and  cfg_dyn[:model_doc]=="content"
+    Settings["cfg_dyn.cmd_doc"] = cmd
+  
+  }
+
+end.parse!
+
+## ARGV is consumed before
+doc=ARGV[0]
+
+d=Dyndoc::TemplateDocument.new(doc)
 
 d.make_all
