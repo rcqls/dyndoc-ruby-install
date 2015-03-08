bfb7dff1019699647a3d1511d9b5999f61312a46
R cqls <rdrouilh@gmail.com>
2015-03-08 19:56:30 +0100

    dyndoc update functions

--- dyndoc_basic_root_structure/bin/msys2/dyndoc.sh
+++ dyndoc_basic_root_structure/bin/msys2/dyndoc.sh
@@ -26,3 +26,84 @@ if [ "$(which pdflatex)" = "" ]; then
   echo "Add it in the environment variable %PATH%"
   echo "Go to: Control Panel -> System -> Advanced system Settings -> Environment Variables"
 fi
+
+## functions to update dyndoc (ruby and R)
+dyn-update-R4rb() {
+  prevdir=$(pwd)
+
+  cd $prevdir
+
+  if [ -d $DYNDOC_HOME/install/ruby ]; then
+  	echo "ruby update: R4rb gem"
+  	cd $DYNDOC_HOME/install/ruby
+  	cd R4rb;git pull;rake install
+  else
+  	echo "ruby update available only when $DYNDOC_HOME/install/ruby exists"
+  fi
+
+  cd ${prevdir}
+}
+
+dyn-update-core() {
+  prevdir=$(pwd)
+
+  cd $prevdir
+
+  if [ -d $DYNDOC_HOME/install/ruby ]; then
+  	echo "ruby update: dyndoc-ruby-core gem"
+  	cd $DYNDOC_HOME/install/ruby
+  	cd dyndoc-ruby-core;git pull;rake install
+  else
+  	echo "ruby update available only when $DYNDOC_HOME/install/ruby exists"
+  fi
+
+  cd ${prevdir}
+}
+
+dyn-update-doc() {
+  prevdir=$(pwd)
+
+  cd $prevdir
+
+  if [ -d $DYNDOC_HOME/install/ruby ]; then
+  	echo "ruby update: dyndoc-ruby-doc gem"
+  	cd $DYNDOC_HOME/install/ruby
+  	cd dyndoc-ruby-doc;git pull;rake install
+  else
+  	echo "ruby update available only when $DYNDOC_HOME/install/ruby exists"
+  fi
+
+  cd ${prevdir}
+}
+
+dyn-update-rb4R() {
+  prevdir=$(pwd)
+
+  cd $prevdir
+
+  if [ -d $DYNDOC_HOME/install/R ]; then
+  	echo "R update: rb4R R package"
+  	cd $DYNDOC_HOME/install/R
+  	cd rb4R;git pull;cd ..
+  	PREFIX_R=""
+  	if [ "$R_LIBS_USER" != ""  ]; then
+  		PREFIX_R="-l $R_LIBS_USER"
+  	fi
+  	R CMD INSTALL $PREFIX_R rb4R
+  else
+  	echo "R update available only when $DYNDOC_HOME/install/R exists"
+  fi
+
+  cd ${prevdir}
+}
+
+dyn-update-ruby() {
+  dyn-update-R4rb
+  dyn-update-core
+  dyn-update-doc
+}
+
+dyn-update-R() {
+  dyn-update-rb4R
+}
+
