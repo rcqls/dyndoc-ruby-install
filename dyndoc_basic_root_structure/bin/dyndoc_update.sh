d19d02b4f1090e0fa6c0b0ae8fa12b8437a5b8d1
R cqls <rdrouilh@gmail.com>
2015-03-11 23:37:01 +0100

    dyndoc_update.sh added

---
+++ dyndoc_basic_root_structure/bin/dyndoc_update.sh
@@ -0,0 +1,95 @@
+## functions to update dyndoc (ruby and R)
+dyn-R4rb-update() {
+  prevdir=$(pwd)
+
+  cd $prevdir
+
+  if [ -d $DYNDOC_HOME/install/ruby ]; then
+  	echo "ruby update: R4rb gem"
+  	cd $DYNDOC_HOME/install/ruby
+  	cd R4rb;git pull;rake install
+  else
+  	echo "ruby update available only when $DYNDOC_HOME/install/ruby exists"
+  fi
+
+  cd ${prevdir}
+}
+
+dyn-ruby-core-update() {
+  prevdir=$(pwd)
+
+  cd $prevdir
+
+  if [ -d $DYNDOC_HOME/install/ruby ]; then
+  	echo "ruby update: dyndoc-ruby-core gem"
+  	cd $DYNDOC_HOME/install/ruby
+  	cd dyndoc-ruby-core;git pull;rake install
+  else
+  	echo "ruby update available only when $DYNDOC_HOME/install/ruby exists"
+  fi
+
+  cd ${prevdir}
+}
+
+dyn-ruby-doc-update() {
+  prevdir=$(pwd)
+
+  cd $prevdir
+
+  if [ -d $DYNDOC_HOME/install/ruby ]; then
+  	echo "ruby update: dyndoc-ruby-doc gem"
+  	cd $DYNDOC_HOME/install/ruby
+  	cd dyndoc-ruby-doc;git pull;rake install
+  else
+  	echo "ruby update available only when $DYNDOC_HOME/install/ruby exists"
+  fi
+
+  cd ${prevdir}
+}
+
+dyn-rb4R-update() {
+  prevdir=$(pwd)
+
+  if [ -d $DYNDOC_HOME/install/R ]; then
+  	echo "R update: rb4R R package"
+  	cd $DYNDOC_HOME/install/R
+  	cd rb4R;git pull;cd ..
+  	PREFIX_R=""
+  	if [ "$R_LIBS_USER" != ""  ]; then
+  		PREFIX_R="-l $R_LIBS_USER"
+  	fi
+  	R CMD INSTALL $PREFIX_R rb4R
+  else
+  	echo "R update available only when $DYNDOC_HOME/install/R exists"
+  fi
+
+  cd ${prevdir}
+}
+
+dyn-ruby-update() {
+  dyn-R4rb-update
+  dyn-ruby-core-update
+  dyn-ruby-doc-update
+}
+
+dyn-R-update() {
+  dyn-rb4R-update
+}
+
+dyn-ruby-bin-update() {
+  prevdir=$(pwd)
+  cd $DYNDOC_HOME/bin
+  wget https://raw.githubusercontent.com/rcqls/dyndoc-ruby-install/master/dyndoc_basic_root_structure/bin/dyndoc-compile.rb
+  wget https://raw.githubusercontent.com/rcqls/dyndoc-ruby-install/master/dyndoc_basic_root_structure/bin/dyndoc-package.rb
+  wget https://raw.githubusercontent.com/rcqls/dyndoc-ruby-install/master/dyndoc_basic_root_structure/bin/dyndoc-server-simple.rb
+  wget https://raw.githubusercontent.com/rcqls/dyndoc-ruby-install/master/dyndoc_basic_root_structure/bin/dyndoc_update.sh
+  wget https://raw.githubusercontent.com/rcqls/dyndoc-ruby-install/master/dyndoc_basic_root_structure/bin/atom_update.sh
+
+  if [[ $MSYSTEM =~ ^MSYS ]]; then
+    cd msys2
+    wget https://raw.githubusercontent.com/rcqls/dyndoc-ruby-install/master/dyndoc_basic_root_structure/bin/msys2/dyndoc.sh
+    wget https://raw.githubusercontent.com/rcqls/dyndoc-ruby-install/master/dyndoc_basic_root_structure/bin/msys2/file_tools.rb
+  fi
+  
+  cd $prevdir
+}
