e00d5a0854b57c7fcc436e57d4df65cf70e0c96a
R cqls <rdrouilh@gmail.com>
2015-03-19 06:54:22 +0100

    fix

--- bin/update_dyndoc.sh
+++ bin/update_dyndoc.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 cd ~/dyndoc/bin
 echo "Download dyndoc_update.sh script inside ~/dyndoc/bin"
+mkdir .tmp
+cd .tmp
 wget https://raw.githubusercontent.com/rcqls/dyndoc-ruby-install/master/dyndoc_basic_root_structure/bin/dyndoc_update.sh
+cp dyndoc_update.sh ..
 . ~/.bash_profile
