c435ad2bd7bd3ec9254196535899744c04b31b17
R cqls <rdrouilh@gmail.com>
2015-03-26 08:23:47 +0100

    pandoc config

---
+++ dyndoc_basic_root_structure/etc/pandoc/config.rb
@@ -0,0 +1,4 @@
+{
+	"md2s5"			=> ["-t","s5","--webtex","-s","-V","s5-url=#{Dyndoc.cfg_dir[:etc]}/pandoc/extra/s5-ui/default", "--self-contained"],
+	"md2revealjs" 	=> ["-t","revealjs","--webtex","-s","-V","revealjs-url=#{Dyndoc.cfg_dir[:etc]}/pandoc/extra/reveal.js-3.0.0", "--self-contained"]
+}
